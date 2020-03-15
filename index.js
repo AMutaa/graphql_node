1dca8da03e17b74b9e13e2924e2779e133273768
AMutaa <piusandru@gmail.com>
2020-03-15 15:38:42 -0400

    added mutations'

--- index.js
+++ index.js
@@ -8,6 +8,36 @@ const resolvers = {
     highlight: (parent, args) => {
       return highlights.find(highlight => highlight.id == args.id);
     }
+  },
+  Mutation: {
+    newHighlight: (parent, args) => {
+      const highlight = {
+        id: String(highlights.length + 1),
+        title: args.title || "",
+        author: args.author || "",
+        content: args.content
+      };
+      highlights.push(highlight);
+      return highlight;
+    },
+    updateHighlight: (parent, args) => {
+      const index = highlights.findIndex(highlight => highlight.id === args.id);
+      const highlight = {
+        id: args.id,
+        content: args.content,
+        author: highlights[index].author,
+        title: highlights[index].author
+      };
+      highlights[index] = highlight;
+      return highlight;
+    },
+    deleteHighlight: (parent, args) => {
+      const deletedHighlight = highlights.find(
+        highlight => highlight.id === args.id
+      );
+      highlights = highlights.filter(highlight => highlight.id !== args.id);
+      return deletedHighlight;
+    }
   }
 };
 
--- schema.js
+++ schema.js
@@ -16,6 +16,12 @@ const typeDefs = gql`
     highlights: [Highlight]!
     highlight(id: ID!): Highlight
   }
+
+  type Mutation {
+    newHighlight(content: String!, title: String, author: String): Highlight!
+    updateHighlight(id: ID!, content: String!): Highlight!
+    deleteHighlight(id: ID!): Highlight!
+  }
 `;
 
 module.exports = typeDefs;
